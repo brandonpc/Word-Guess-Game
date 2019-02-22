7f47d3503caf2b127e27fa674b28f9a1e7828274
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-02-21 23:05:43 -0700

    skeleton from psychic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,60 @@
 //Let's Make a Game
 
 //game displays blank word
-
-var lozNamesBank["zelda", "link"];
-var lozName = lozNamesBank[Math.floor(Math.random() * lozNamesBank.length)];
-
-
 // **Global variables**
+var lozNamesBank = ["zelda", "link", ];
+
 var wins = 0;
 var losses = 0;
-// var guessesLeft = 15;
-// var lettersGuessed = [];
-var blankWord = [];
+var guessesLeft = 15;
+var lettersGuessed = [];
+// var blankWord = [];
 
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
 
-    function wordChoice()[
-        for (var i = 0; i < .length; i++) {
-            blankWord[i] = "_";
-        }
-    ]
+    var computerChoice = lozNamesBank[Math.floor(Math.random() * lozNamesBank.length)];
 
-    // if (guessesLeft < 0) {
-    //     losses++
-    // }
+    // are userLetters tied to userGues???
+    var userLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+        "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    ];
 
-    if (wordChoice === "link" && (userGuess = "l" && "i" && "n" && "k") {
-            wins++
-        }
-    }
-
-    if (wordChoice === "zelda" && (userGuess = "z" && "e" && "l" && "d" && "a") {
-            wins++
-        }
+    //convert my strings to arrays, use for loops to find correct letters. create action for each letter.
+    // refresh: functions, methods, properties of strings, 
+    // need to replace "String" with a placeholder/each individual string (create a method?)
+    for (i = 0; string.length; i++) {
+        // if userGuess === (letter(s) in word)
+        //     guessChoices.push(userGuess); (letter guess logged to guessChoces)
+        //     numGuesses--
+        //     
     }
 
 
+    if (letters.indexOf(userGuess)) {
+        if (userGuess === computerChoice) {
+            wins++;
+            numGuesses = 9;
+            guessChoices = [];
+        }
+
+        if (userGuess != computerGuess) {
+            numGuesses--;
+            guessChoices.push(userGuess);
+        }
+
+        if (numGuesses === 0) {
+
+            numGuesses = 9;
+            losses++;
+            guessChoices = [];
+        }
+    }
 }
 
-//# of guesses left (15?)
-
-//user types any key
-//letter is recorded in "Already guessed"
-
-//if correct, letter appears in word
-//else??
-
-//if all letters = word 
-//increase wins++
-//else losses++
+// function wordChoice()[
+//     for (var i = 0; i < .length; i++) {
+//         blankWord[i] = "_";
+//     }
+// ]
